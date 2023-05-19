f2ae28f7e29e2d4714c8506997bd37da4740e8b7
Lars W. Andersen <lars@factus.dk>
2023-05-19 11:38:49 +0200

    123

--- scripts/linuxguests/RKE2InstallscriptCP.sh
+++ scripts/linuxguests/RKE2InstallscriptCP.sh
@@ -6,13 +6,6 @@ sudo cp /etc/rancher/rke2/rke2.yaml /home/vagrant/rke2vagrantkubeconfig
 sudo chown vagrant:vagrant /home/vagrant/rke2vagrantkubeconfig
 
 
-# install kubectl
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
-
-
 # sudo journalctl -u rke2-server -f
 
 # sudo /var/lib/rancher/rke2/bin/kubectl get node --kubeconfig=/etc/rancher/rke2/rke2.yaml
--- scripts/linuxguests/ubuntu_nodeprereq.sh
+++ scripts/linuxguests/ubuntu_nodeprereq.sh
@@ -3,3 +3,10 @@ sudo swapoff -a
 sudo sed -i.bak '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab  #Turning off swap permanently through fstabsdocker
 mkdir -p /etc/rancher/rke2
 sudo cp /home/vagrant/config.yaml /etc/rancher/rke2/config.yaml
+
+
+# install kubectl
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
