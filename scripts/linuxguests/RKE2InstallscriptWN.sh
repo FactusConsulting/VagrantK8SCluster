70120ba52a18032129d13d0ff2eeefbb896cac0e
lars <lars@factus.dk>
2023-05-24 14:16:36 +0200

    123

--- scripts/linuxguests/RKE2InstallscriptWN.sh
+++ scripts/linuxguests/RKE2InstallscriptWN.sh
@@ -2,6 +2,7 @@ curl -sfL https://get.rke2.io | sudo INSTALL_RKE2_TYPE="agent" sh -
 
 sudo systemctl enable rke2-agent.service
 sudo systemctl start rke2-agent.service
+sudo ln -s /var/lib/rancher/rke2/bin/kubectl /usr/local/bin/kubectl
 
 
 # sudo journalctl -u rke2-agent -f
--- scripts/linuxguests/ubuntu_nodeprereq.sh
+++ scripts/linuxguests/ubuntu_nodeprereq.sh
@@ -4,6 +4,11 @@ sudo sed -i.bak '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab  #Turning off swap perman
 mkdir -p /etc/rancher/rke2
 sudo cp /home/vagrant/config.yaml /etc/rancher/rke2/config.yaml
 
-
 sudo systemctl stop ufw
 sudo systemctl disable ufw
+
+mkdir $HOME/.kube
+echo 'export KUBECONFIG="$HOME/.kube/config"' >> ~/.bashrc
+echo "alias k='kubectl'" >> ~/.bashrc
+sudo cp -i /etc/rancher/rke2/rke2.yaml $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
