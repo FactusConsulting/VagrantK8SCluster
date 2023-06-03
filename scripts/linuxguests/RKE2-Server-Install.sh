164f1307d3fac2844ae6af88967795c1b1df8c10
lars <lars@factus.dk>
2023-06-04 00:22:00 +0200

    rocky now working

--- scripts/linuxguests/RKE2-Server-Install.sh
+++ scripts/linuxguests/RKE2-Server-Install.sh
@@ -1,7 +1,15 @@
 curl -sfL https://get.rke2.io | sudo INSTALL_RKE2_TYPE=server sh -
-sudo systemctl enable rke2-server.service
-sudo systemctl start rke2-server.service
 
+sudo cp -f /usr/share/rke2/rke2-cis-sysctl.conf /etc/sysctl.d/60-rke2-cis.conf
+sudo systemctl restart systemd-sysctl
+
+if grep -q 'Rocky Linux' /etc/os-release; then
+  echo "This is a Rocky Linux machine."
+  sudo cp -f /usr/share/rke2/rke2-cis-sysctl.conf /etc/sysctl.d/60-rke2-cis.conf
+  sudo systemctl restart systemd-sysctl
+else
+  echo "This is not a Rocky Linux machine."
+fi
 
 # Only copy the kubeconfig back to the host when its the cp11 server. The first controlplane
 hostname=$(hostname)
