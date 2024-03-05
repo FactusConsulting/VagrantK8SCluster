50ace7b52f684ba34aebff695bccf880bdda1f06
Lars W. Andersen <lars@factus.dk>
2024-03-05 22:49:02 +0100

    Added WSL2 support

--- scripts/linuxguests/RKE2-Server-Install.sh
+++ scripts/linuxguests/RKE2-Server-Install.sh
@@ -1,11 +1,11 @@
 curl -sfL https://get.rke2.io | sudo INSTALL_RKE2_TYPE=server sh -
 
-sudo cp -f /usr/share/rke2/rke2-cis-sysctl.conf /etc/sysctl.d/60-rke2-cis.conf
+sudo cp -f /usr/local/share/rke2/rke2-cis-sysctl.conf /etc/sysctl.d/60-rke2-cis.conf
 sudo systemctl restart systemd-sysctl
 
 if grep -q 'Rocky Linux' /etc/os-release; then
   echo "This is a Rocky Linux machine."
-  sudo cp -f /usr/share/rke2/rke2-cis-sysctl.conf /etc/sysctl.d/60-rke2-cis.conf
+  sudo cp -f /usr/local/share/rke2/rke2-cis-sysctl.conf /etc/sysctl.d/60-rke2-cis.conf
   sudo systemctl restart systemd-sysctl
 else
   echo "This is not a Rocky Linux machine."
