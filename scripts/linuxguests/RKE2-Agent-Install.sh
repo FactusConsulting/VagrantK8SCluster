659db41ba12548e97744ab8442be68893d41ab3f
Larswa <lars@factus.dk>
2024-03-06 08:19:31 +0100

    Path updates from changes in RKE2 master installscript.

--- scripts/linuxguests/RKE2-Agent-Install.sh
+++ scripts/linuxguests/RKE2-Agent-Install.sh
@@ -2,7 +2,7 @@ curl -sfL https://get.rke2.io | sudo INSTALL_RKE2_TYPE="agent" sh -
 
 if grep -q 'Rocky Linux' /etc/os-release; then
   echo "This is a Rocky Linux machine."
-  sudo cp -f /usr/share/rke2/rke2-cis-sysctl.conf /etc/sysctl.d/60-rke2-cis.conf
+  sudo cp -f /usr/local/share/rke2/rke2-cis-sysctl.conf /etc/sysctl.d/60-rke2-cis.conf
   sudo systemctl restart systemd-sysctl
 else
   echo "This is not a Rocky Linux machine."
